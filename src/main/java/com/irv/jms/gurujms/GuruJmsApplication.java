8ea9f1b28f49fa7743bbb3868e73aae95a81fd92
irvcal <juanirvincalderon@gmail.com>
2022-03-22 10:45:25 -0600

    First commit

---
+++ src/main/java/com/irv/jms/gurujms/GuruJmsApplication.java
@@ -0,0 +1,13 @@
+package com.irv.jms.gurujms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irv/jms/gurujms/GuruJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irv.jms.gurujms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
